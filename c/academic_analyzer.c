e5b582b21549e0efce9bedf366127be911a37f57
Henrique Verissimo <henryestudosp@gmail.com>
2026-01-31 18:33:15 -0300

    c-01(main): Create a loop for capture the number of studants
    - Add a "do while" loop to ensure a valid number of students.

--- c/academic_analyzer.c
+++ c/academic_analyzer.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-
 int main(void) {
     float testScore1, testScore2, average;
     char status[15];
     int students;
 
+    do {
+        printf("Digite a quantidade de alunos: ");
+        scanf("%d", &students);
 
+        if (students < 0) {
+            printf("O número de estudantes não pode ser negativo!\n\n");
+        }
+    } while (students < 0);
 
     return 0;
 }
