f7f9572bf29597f99585bd0a0aa0cd1d5545ac37
Henrique Verissimo <henryestudosp@gmail.com>
2026-02-01 16:36:37 -0300

    bash-01(docs): Create script header
    - Add a header with some information about the script.

---
+++ bash/01/organizer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+# Script_name: Organizador de Workspace.
+# Description: Um script que organiza arquivos .log e .tmp no diretório.
+# Author: Henrique Verissimo
+# Version: 0.1.0
+# Date: 01/02/2026
+# License: GPL (No Warranty)
+# Contact:
+#   - henriqueverissimocontato@gmail.com
+#   - https://github.com/HenryVerissimo
+#   - https://www.linkedin.com/in/henrique-verissimo
+#-------------------------------------------------------------------------
+# Usage: ./organizer.sh ou bash organizer.sh
+#-------------------------------------------------------------------------
+# Variables:
+#-------------------------------------------------------------------------
+# Script:
