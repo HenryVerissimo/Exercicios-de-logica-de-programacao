49b01e885f12062b2ec0f83ab836b2e7016b8fc5
Henrique Verissimo <henryestudosp@gmail.com>
2026-02-02 22:37:08 -0300

    python-01(interface): Refactor some validates
    - reinforces user data validations

--- python/01/program_interface.py
+++ python/01/program_interface.py
@@ -2,7 +2,7 @@ from utils import clear_terminal
 
 def interface() -> None | dict:
 
-    clear_terminal()
+    clear_terminal(2)
 
     while True:
         print("Simulação de cadastro de usuários\n")
@@ -13,19 +13,25 @@ def interface() -> None | dict:
 
         match option:
             case "1":
-                name = input("\nDigite o nome: ")
+                while True:
+                    name = input("\nDigite o nome: ")
+
+                    if(len(name) >= 3):
+                        break
+
+                    print("Erro: O nome deve ter pelo menos 3 caracteres")
                 
                 while True:
                     try:
                         age = int(input("Digite a idade: "))
 
-                        if ((age >= 0) and (age <= 100)):
+                        if ((age >= 18) and (age <= 100)):
                             break
 
-                        print("Erro! Você precisa digitar uma idade entre 0 e 100.")
+                        print("Erro: Você precisa digitar uma idade entre 18 e 100.\n")
 
                     except ValueError:
-                        print("Erro! Você precisa digitar uma idade válida.")
+                        print("Erro: A idade precisa ser um válor númerico.\n")
                 
                 while True:
                     email = input("Digite o email: ")
@@ -36,10 +42,10 @@ def interface() -> None | dict:
                         if (email_split[1].count(".") >= 1):
                             break
                         else:
-                            print("Email inválido! O email deve conter um '.' após o '@'.")
+                            print("Erro: Email inválido! O email deve conter um '.' após o '@'.\n")
                     else:
 
-                        print("Email inválido! O email deve conter um (e apenas um) '@' em seu corpo.")
+                        print("Erro: Email inválido! O email deve conter um (e apenas um) '@' em seu corpo.\n")
                 
                 while True:
                     admin = input("O usuário é admin (S/N)? ")
@@ -47,7 +53,7 @@ def interface() -> None | dict:
                     if (admin.upper() in ["S", "N"]):
                         break
 
-                    print("Opção inválida! Escolha entre S e N.")
+                    print("Erro: Opção inválida! Escolha entre S e N.\n")
 
                 return {"name": name, "age": age, "email": email, "admin": admin}
             
@@ -56,6 +62,6 @@ def interface() -> None | dict:
                 return None
 
             case _:
-                print("\nOpção inválida! Tente novamente...", end="")
+                print("\nErro: Opção inválida! Tente novamente...", end="")
                 clear_terminal(sleep_seconds=2)
                 continue
